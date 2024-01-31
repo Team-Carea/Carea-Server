037f6e20a0bf407e1f3050144e0663232f47f7a8
ilu25 <acejsy@gmail.com>
2024-02-01 01:00:16 +0900

    Feat: 채팅 기능 구현 (#2)
    - 웹 소켓을 이용하여 실시간 채팅 가능
    - 채팅 기록 MySQL DB에 저장 및 불러오기 가능
    - HCJ로 구현한 UI는 차후 플러터 도입 시 삭제 예정

--- carea/chats/models.py
+++ carea/chats/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from helps.models import Help
+from users.models import User
+
+class ChatRoom(models.Model):
+    help = models.ForeignKey(Help, on_delete=models.PROTECT)
+    helped = models.IntegerField()   # 도움 요청자 id
+    helper = models.ForeignKey(User, on_delete=models.PROTECT)
+
+class Chat(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    message = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
--- carea/comments/models.py
+++ carea/comments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from posts.models import Post
+from users.models import User
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.PROTECT)
+    content = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
