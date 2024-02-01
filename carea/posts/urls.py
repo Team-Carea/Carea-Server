fe369c7850df77acd20d85bf0997945e57cb3a48
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-01 21:55:48 +0900

    Feat: urls.py 경로 변경
    1. posts/post 경로로 왔을 때, views.py의 write_post 함수가 실행되도록 함

--- carea/posts/urls.py
+++ carea/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test_page),
+    path('post/', views.write_post),
     path('<str:category>/', views.view_category)
     # <str:category> = 문자열 타입의 동적 변수
 ]
