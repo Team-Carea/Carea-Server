e0933c7d9c8ffd76fdc39b4a522a070f2eff78f4
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-02 20:26:54 +0900

    Feat: 목록 창에서 카테고리 값 띄우기
    1. 게시물 작성 후, 원래 있었던 카테고리 목록 페이지로 돌아가도록 함
    2. category_list 클래스 내에 카테고리 단일 값만을 출력하는 함수(get_category_value) 생성
    3. 2번의 함수를 통해 게시물 목록에서 올바른 카테고리 값을 출력하도록 함.

--- carea/posts/views.py
+++ carea/posts/views.py
@@ -6,17 +6,25 @@ from users.models import User
 
 # 카테고리별 게시물 목록을 가져오기 위한 클래스 생성
 class category_list:
-    def get_category(self, category):
+    # 카테고리 내 모든 게시물
+    def get_category_list(self, category):
         # DB에서 카테고리를 기준으로 값을 가져온다.
         lists = Post.objects.filter(category=category)
         # category 값이 latest, 즉 최신글인 경우 모든 게시물을 가져온다.
         if(category == 'latest') :
             lists = Post.objects.all()
         return lists
+
+    # 카테고리 단일 값
+    def get_category_value(self, category):
+        return category
+
 def view_category(request, category) :
     category_instance = category_list()
-    category_value = category_instance.get_category(category)
-    return render(request, 'category_select.html', {'category': category_value})
+    category_posts = category_instance.get_category_list(category)
+    global category_value
+    category_value = category_instance.get_category_value(category)
+    return render(request, 'category_select.html', {'category' : category_posts, 'category_value': category_value})
 
 # 게시물 작성을 위한 클래스 생성
 class PostInfo:
@@ -37,9 +45,7 @@ def write_post(request):
     post_instance = PostInfo()
     if request.method == "POST":
         post_instance.post(request)
-        # 이전 페이지로 돌아갈 때 필요한 category 정보 가져오기
-        previous_category = request.POST.get('category')
-        # 이전 페이지(카테고리 목록)로 돌아가기
-        return view_category(request, previous_category)
+        # 이전 페이지로 돌아가기
+        return view_category(request, category_value)
     else:
         return post_instance.get(request)
