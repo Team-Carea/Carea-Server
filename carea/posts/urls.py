22fc74765573cbaac98bf7303bb5caa6cda0f722
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-03 21:40:57 +0900

    Feat: urls.py에 post_id 경로 추가
    1. 게시물 상세보기 기능을 위해 포스트 아이디 값을 경로로서 받도록 함

--- carea/posts/urls.py
+++ carea/posts/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('post/', views.write_post, name="write_post"),
+    path('<int:post_id>/', views.view_detail, name="view_detail"),
+    # <bigint:post_id> = 정수형 타입의 동적 변수
     path('<str:category>/', views.view_category, name="view_category")
     # <str:category> = 문자열 타입의 동적 변수
 ]
