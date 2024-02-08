9039f7da9f33b2281091d488e9ff4d51dca01060
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-09 02:32:29 +0900

    Feat: posts 앱 url 수정
    1. posts/ 경로 삭제
    2. 경로에 카테고리 값이 있을 경우 views.py의 category_page 함수 실행 (여기서 GET의 경우와 POST의 경우를 나눔)
    3. 각 URL 이름과 views.py와 연결할 함수 이름을 명사형으로 수정

--- carea/posts/urls.py
+++ carea/posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.urls import include
 from . import views
 
 urlpatterns = [
-    path('post/', views.write_post, name="write_post"),
-    path('<int:post_id>/', views.view_detail, name="view_detail"),
+    path('', views.categories, name="categories"),
+    path('<int:post_id>/', views.detail, name="detail"),
     # <bigint:post_id> = 정수형 타입의 동적 변수
-    path('<str:category>/', views.view_category, name="view_category")
+    path('<str:category>/', views.category_page, name="category_page"),
     # <str:category> = 문자열 타입의 동적 변수
 ]
