17495ff2e33d7844c0edfa83c2b26775049f4b32
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-11 16:50:55 +0900

    Feat: User 모델 수정
    1. models.Model을 통해 정의한 클래스를 AbstractUser로 정의
    2. settings.py에 AUTH_USER_MODEL 값 추가

--- carea/users/models.py
+++ carea/users/models.py
@@ -1,10 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, UserManager
 # Create your models here.
-class User(models.Model):
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
     nickname = models.CharField(max_length=10)
-    email = models.CharField(max_length=20)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nickname']
+
+    objects = UserManager()
+
+    # 필요한 필드 추가
     profile_url = models.CharField(max_length=255, blank=True, null=True)
     introduction = models.CharField(max_length=100, blank=True, null=True)
     point = models.IntegerField(default=0)
-    token = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.email
