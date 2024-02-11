5d2bdfe1fb88916d9ea000ee6aafff6343770699
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-11 16:56:38 +0900

    Feat: posts앱 url 수정
    1. comment 기능을 위한 URL을 추가
    (이전에는 views.py의 detail 함수 내에서comments앱을 import해 댓글 기능을 구현하려 함)

--- carea/posts/urls.py
+++ carea/posts/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.categories, name="categories"),
-    path('<int:post_id>/', views.detail, name="detail"),
+    path('<int:post_id>/comment/', include('comments.urls')),
     # <bigint:post_id> = 정수형 타입의 동적 변수
+    path('<int:post_id>/', views.detail, name="detail"),
     path('<str:category>/', views.category_page, name="category_page"),
     # <str:category> = 문자열 타입의 동적 변수
 ]
