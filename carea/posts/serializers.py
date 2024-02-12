763aa5c9c9eb47bfaad1821dfd2fbe2e37e0dfd9
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-12 22:36:31 +0900

    Feat: posts앱 새로운 컬럼/필드 생성
    1. models.py를 통해 DB에 'user_info' 컬럼 생성
    2. serializers.py에 user_info 필드 생성

--- carea/posts/serializers.py
+++ carea/posts/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Post
+from users.serializers import UserSerializer
+from users.models import User
+
+class CustomUserSerializer(UserSerializer):
+    class Meta:
+        model = User
+        # 필요한 필드만을 지정
+        fields = ['nickname']
 
 class PostSerializer(serializers.ModelSerializer):
+    #user의 특정 정보 (닉네임 등)
+    user_info = CustomUserSerializer(read_only=True)
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'category', 'created_at', 'updated_at', 'user']
+        fields = ['id', 'title', 'content', 'category', 'created_at', 'updated_at', 'user', 'user_info']
