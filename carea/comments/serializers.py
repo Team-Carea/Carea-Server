e461f5f4cae96308d7ad6dcfb21649a9019aa694
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-14 23:00:10 +0900

    Remove: 커뮤니티앱 serializers.py 필드 수정
    1. user_info 필드 삭제
    2. 임시용 UserSerializer 대신 CustomUserDetailsSerailizer를 임포트

--- carea/comments/serializers.py
+++ carea/comments/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from .models import Comment
-from users.serializers import UserSerializer
 from users.models import User
+from django.shortcuts import get_object_or_404
 
-class CustomUserSerializer(UserSerializer):
+class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         # 필요한 필드만을 지정
@@ -11,7 +11,7 @@ class CustomUserSerializer(UserSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     #user의 특정 정보 (닉네임 등)
-    user_info = CustomUserSerializer(read_only=True)
+    user = CustomUserSerializer(read_only=True)
     class Meta:
         model = Comment
-        fields = ['id', 'post', 'content', 'user', 'user_info', 'created_at']
+        fields = ['id', 'post_id', 'content', 'created_at','user']
--- carea/posts/serializers.py
+++ carea/posts/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from .models import Post
-from users.serializers import UserSerializer
+from users.serializers import CustomUserDetailsSerializer
 from users.models import User
 
-class CustomUserSerializer(UserSerializer):
+class CustomUserSerializer(CustomUserDetailsSerializer):
     class Meta:
         model = User
         # 필요한 필드만을 지정
@@ -11,7 +11,7 @@ class CustomUserSerializer(UserSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     #user의 특정 정보 (닉네임 등)
-    user_info = CustomUserSerializer(read_only=True)
+    user= CustomUserSerializer(read_only=True)
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'category', 'created_at', 'updated_at', 'user', 'user_info']
+        fields = ['id', 'title', 'content', 'category', 'created_at', 'updated_at', 'user']
