6faf12cb8264ccbd20a47bef9c4ed607290185b0
ilu25 <acejsy@gmail.com>
2024-02-14 19:39:43 +0900

    Feat: STT 도움 인증에 권한 부여
    - JWT 미들웨어를 추가하여 웹소켓 연결 시 쿼리 스트링에 token 필요
    - 연결 성공 시 컨슈머의 scope에 유저 객체 저장

--- carea/helps/routing.py
+++ carea/helps/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_helps_urlpatterns = [
-    re_path(r"ws/helps/stt/(?P<room_id>\w+)/$", consumers.HelpConsumer.as_asgi()),
+    re_path(r"ws/helps/stt/(?P<room_id>\w+)$", consumers.HelpConsumer.as_asgi()),
 ]
