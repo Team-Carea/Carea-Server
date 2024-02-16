ed2c47fab4c8fde9b59f3b2fd87c55f3b5356777
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-16 14:58:29 +0900

    Refactor: comments앱 URL 수정
    1. path('int:post_id>/comment를 복수형인 comments로 수정

--- carea/posts/urls.py
+++ carea/posts/urls.py
@@ -3,7 +3,7 @@ from django.urls import include
 from . import views
 
 urlpatterns = [
-    path('<int:post_id>/comment/', include('comments.urls')),
+    path('<int:post_id>/comments/', include('comments.urls')),
     # <bigint:post_id> = 정수형 타입의 동적 변수
     path('<int:post_id>/', views.detail, name="detail"),
     path('<str:category>/', views.category_page, name="category_page"),
