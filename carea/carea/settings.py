dfc3f1cfd217f2796f8a95f6182b25ab0439aaf3
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-16 14:53:16 +0900

    Remove: settings.py 중복 요소 제거
    1. INSTALLED_APP에서 rest_framework 삭제
    2. AUTH_USER_MODEL = 'users.User' 삭제

--- carea/carea/settings.py
+++ carea/carea/settings.py
@@ -67,8 +67,7 @@ INSTALLED_APPS = [
     'helps',
     'posts',
     'users',
-    'comments',
-    'rest_framework'
+    'comments'
 ]
 
 SITE_ID = 1
@@ -203,5 +202,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'users.User'
