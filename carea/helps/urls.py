e2a01cdfb4d583da3689e56591e98819295efb0c
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-17 22:14:32 +0900

    Feat: 도움요청 조회, 작성 기능 구현
    - 메인페이지에서 도움요청글 마커가 찍힐 수 있도록 위도,경도 전달
    - 위치정보(주소)를 받으면 위도,경도로 변환하는 geocode 함수 구현
    - help_id를 받으면 요청글의 상세정보 출력

--- carea/helps/urls.py
+++ carea/helps/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
+    # 메인 화면
+    path('', views.maps, name="map"),
+    # 상세 게시물
+    path('<int:help_id>', views.helps, name="help_post"),
     # 랜덤 인증 문장 받고, 음성 파일 보낼 수 있는 화면
-    path("stt", views.stt, name="stt"),
+    path("stt", views.stt, name="stt")
 ]
