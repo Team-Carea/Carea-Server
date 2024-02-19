50770212925cbd15a782732f61dc9bc42a5ba5bb
Nyeong <iwbm312@gmail.com>
2024-02-20 02:05:44 +0900

    Chore: add allowed host
    - 안드로이드 로컬호스트 추가
    - 도커 컨테이너 호스트 주소 처리를 위해 '*' 추가

--- carea/carea/settings.py
+++ carea/carea/settings.py
@@ -33,8 +33,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '35.227.187.166', '10.0.2.2']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '35.227.187.166', '10.0.2.2', '*']
 
 # Application definition
 
