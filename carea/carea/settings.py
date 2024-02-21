66e5aaa9011d159677b6761ccb8e1e70f680c093
ilu25 <acejsy@gmail.com>
2024-02-21 16:11:05 +0900

    Chore: settings.py에서 디버깅 모드 환경변수 처리 취소
    - Boolean은 환경변수에서 먹히지 않아 그냥 코드에서 True로 처리 (배포용에서는 False로 바꿈)

--- carea/carea/settings.py
+++ carea/carea/settings.py
@@ -31,7 +31,7 @@ pymysql.install_as_MySQLdb()
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DJANGO_DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '35.227.187.166', '10.0.2.2', '*']
 
