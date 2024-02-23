f9696d4791fe752afdef08bb7156e5ede7815bf6
임지영 <141422840+naneunimjee@users.noreply.github.com>
2024-02-23 22:46:02 +0900

    Refactor: helps앱 urls.py 수정
    1. URL 경로에 '/' 추가

--- carea/helps/urls.py
+++ carea/helps/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # 메인 화면
     path('', views.maps, name="map"),
     # 상세 게시물
-    path('<int:help_id>', views.helps, name="help_post"),
+    path('<int:help_id>/', views.helps, name="help_post"),
     # 랜덤 인증 문장 받고, 음성 파일 보낼 수 있는 화면
     path("stt", views.stt, name="stt")
 ]
